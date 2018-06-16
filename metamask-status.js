1d299205c2593dc09bd76a4fe3d86535856be159
Anatoli Babenia <anatoli@rainforce.org>
2018-06-16 12:12:25 +0700

    Update metamask-status.js
    Add web3 + MetaMask detection with logging to console

--- metamask-status.js
+++ metamask-status.js
@@ -4,5 +4,21 @@ class MetamaskStatus extends HTMLElement {
   constructor() {
     super();
   }
+  
+  connectedCallback() {
+    // check web3 and MetaMask specifically
+    window.addEventListener('load', function() {
+      if (typeof web3 !== 'undefined') {
+        console.log('web3 is enabled')
+        if (web3.currentProvider.isMetaMask === true) {
+          console.log('MetaMask is active')
+        } else {
+          console.log('MetaMask is not available')
+        }
+      } else {
+        console.log('web3 is not found')
+      }
+    })
+  }
 }
 customElements.define('metamask-status', MetamaskStatus)
