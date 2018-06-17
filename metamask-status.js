5d38b9cff91419ff63cffb0d43a40e0f995009a9
Anatoli Babenia <anatoli@rainforce.org>
2018-06-17 12:43:21 +0700

    Update metamask-status.js
    Set and remove HTML attributes `enabled` and `unlocked`

--- metamask-status.js
+++ metamask-status.js
@@ -11,13 +11,17 @@ class MetamaskStatus extends HTMLElement {
       if (typeof web3 !== 'undefined') {
         console.log('web3 is enabled')
         if (web3.currentProvider.isMetaMask === true) {
+          this.setAttribute('enabled', '')
           if (web3.eth.coinbase === null) {
             console.log('MetaMask is locked')
+            this.removeAttribute('unlocked', '')
           } else {
             console.log('MetaMask is unlocked')
+            this.setAttribute('unlocked', '')
           }
         } else {
           console.log('MetaMask is not available')
+          this.removeAttribute('enabled', '')
         }
       } else {
         console.log('web3 is not found')
